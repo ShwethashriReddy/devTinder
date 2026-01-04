1ebd4f9810b67557283838d0a283a4d80ee18a57
SwethasriReddy <sreddy@symplr.com>
2026-01-04 15:11:39 +0530

    commit

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+/**devtinder */
 
 const userSchema = new mongoose.Schema(
   {
